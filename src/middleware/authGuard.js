c0b0c0644f86cdbeaac864fe39eaf7dc2b2706a0
sakib-xrz <sakibxrz21@gmail.com>
2024-04-17 00:09:01 +0600

    add auth guard middleware

---
+++ src/middleware/authGuard.js
@@ -0,0 +1,40 @@
+import jwt from "jsonwebtoken";
+
+import { config } from "../config/config.js";
+import ApiError from "../error/ApiError.js";
+
+const authGuard = (...roles) => {
+  async (req, _res, next) => {
+    try {
+      // get token from request headers
+      const bearerToken = req.headers.authorization;
+
+      // check if token exists
+      if (!bearerToken) {
+        throw new ApiError(401, "You are not authorized to access this route");
+      }
+
+      // extract token from bearer token
+      const token = bearerToken.split(" ")[1];
+
+      const secret = config.jwtSecret;
+
+      // verify token
+      const decoded = jwt.verify(token, secret);
+      req.user = decoded;
+
+      // check if user has the required role
+      if (roles.length && !roles.includes(req.user.role)) {
+        throw new ApiError(
+          403,
+          "You don't have permission to access this route"
+        );
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+export default authGuard;
